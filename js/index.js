3d5c709f2947d4a53309483febdab13fd667a34b
Diana Grams <deegrams221@gmail.com>
2019-08-06 12:55:52 -0600

    created a click event on all paragraphs

--- js/index.js
+++ js/index.js
@@ -2,6 +2,19 @@
 
 // mouseover
 const logoHeading = document.querySelector('.logo-heading');
-logoHeading.addEventListener("mouseover", (e) => e.target.style.transform = "scale(1.5)");
-logoHeading.addEventListener("mouseout", (e) => e.target.style.transform = "scale(1)");
+logoHeading.addEventListener("mouseover", (e) => {
+    e.target.style.transform = "scale(1.5)";
+});
 
+// mouseout
+logoHeading.addEventListener("mouseout", (e) => {
+    e.target.style.transform = "scale(1)";
+});
+
+// click
+const paragraph = document.querySelectorAll('p');
+paragraph.forEach((elementP) => {
+    elementP.addEventListener("click", (e) => {
+        e.target.style.color = "green";
+    });
+});
