605fb942cf1f835925a4f61414d51da2643ad0e4
Diana Grams <deegrams221@gmail.com>
2019-08-06 13:52:06 -0600

    created a scroll event on the window

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
 // Your code goes here
 
-// mouseover
+// 1. mouseover
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener("mouseover", (e) => {
     e.target.style.transform = "scale(1.5)";
 });
 
-// mouseout
+// 2. mouseout
 logoHeading.addEventListener("mouseout", (e) => {
     e.target.style.transform = "scale(1)";
 });
 
-// click
-const paragraph = document.querySelectorAll('p');
-paragraph.forEach((elementP) => {
+// 3. click
+const paragraphs = document.querySelectorAll('p');
+paragraphs.forEach((elementP) => {
     elementP.addEventListener("click", (e) => {
         e.target.style.color = "green";
     });
+});
+
+// 4. scroll
+window.addEventListener("scroll", (e) => {
+    document.body.style.opacity = "0.3";
+    setTimeout(() => {
+        document.body.style.opacity = "1";
+    }, 700);
 });
