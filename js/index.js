e27f03fa0ca50821e6ddc374224ca011fca764ba
Diana Grams <deegrams221@gmail.com>
2019-08-06 12:28:30 -0600

    created mouseover and mouseout events for the logo-heading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+// mouseover
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener("mouseover", (e) => e.target.style.transform = "scale(1.5)");
+logoHeading.addEventListener("mouseout", (e) => e.target.style.transform = "scale(1)");
+
